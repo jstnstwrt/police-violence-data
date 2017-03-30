3870519ccae32d35335ca6e5479b0ded94c028ea
jstnstwrt <justinstewart.la@gmail.com>
2017-03-30 15:09:51 -0400

    correcting typo

--- police-violence-data.wsgi
+++ police-violence-data.wsgi
@@ -6,4 +6,4 @@ logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, '/var/www/html/police-violence-data')
 print(sys.executable)
 
-from flask_app import app as application
+from police-violence-data import app as application
--- police-violence-data.wsgi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-import logging
-
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, '/var/www/html/police-violence-data')
-print(sys.executable)
-
-from police-violence-data import app as application
