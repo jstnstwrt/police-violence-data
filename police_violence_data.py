945cccf6ee5888373c5a6685256b7d024b076c73
jstnstwrt <justinstewart.la@gmail.com>
2017-03-30 15:53:51 -0400

    added api endpoint to serve fresh data via get request

--- police_violence_data.py
+++ police_violence_data.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+
+
+data_store = {'incidents' : [
+	{'name':'sally','age':15,'gender':'m','img_url':'http://cdn-r2.unilad.co.uk/wp-content/uploads/2015/12/person-of-the-year-2.jpg'},
+	{'name':'mike','age':45,'gender':'f','img_url':'http://i.telegraph.co.uk/multimedia/archive/03491/Vladimir_Putin_1_3491835k.jpg'},
+	{'name':'justin','age':25,'gender':'m','img_url':'http://images.amcnetworks.com/amc.com/wp-content/uploads/2015/05/MM_714_JM_0623_1545.jpg'},
+	{'name':'jim','age':99,'gender':'m','img_url':'https://i.kinja-img.com/gawker-media/image/upload/s--LJ4kR8Aa--/c_scale,fl_progressive,q_80,w_800/197gkt72jr0e1jpg.jpg'}
+	]}
+
+
+def grab_incidents(zipcode):
+	## does real work
+	return data_store
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/police_data", methods=['GET'])
+def get_police_data():
+	zipcode = request.args.get('zipcode')
+	incidents = grab_incidents(zipcode)
+	return jsonify(incidents)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
