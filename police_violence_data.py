7d5add86de31ca87626352a4ededb980b6d9aa58
jstnstwrt <justinstewart.la@gmail.com>
2017-03-30 17:49:55 -0400

    changing the path

--- police_violence_data.py
+++ police_violence_data.py
@@ -12,7 +12,7 @@ data_store = {'incidents' : [
 	{'name':'jim','age':99,'gender':'m','img_url':'https://i.kinja-img.com/gawker-media/image/upload/s--LJ4kR8Aa--/c_scale,fl_progressive,q_80,w_800/197gkt72jr0e1jpg.jpg'}
 	]}
 
-df = pd.read_csv('police_killings_clean.csv')
+df = pd.read_csv('~/police-violence-data/police_killings_clean.csv')
 
 # Zipcodes is a list, e.g., [11238,90601]
 def get_incidents(zipcodes):
